505d254538fbebe41eec17e19291e3c98379f9ba
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2024-02-26 22:03:06 +0200

    link of welcome page is on the center now

--- src/Pages/WelcomePage/WelcomePage.js
+++ src/Pages/WelcomePage/WelcomePage.js
@@ -5,11 +5,10 @@ export const WelcomePage =()=>{
 return(
 
 <div  style={{
-    display:'inline-block',
-        width: '100%',
-        minHeight: '100vh',
-     
-        left: '50%'
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
       }}>
     <NavLink to='/catalog'>Get your car</NavLink>   
 </div>
